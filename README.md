b44676b1b719439b0d5544c03b472239da829887
jsnow7 <15jasnow@gmail.com>
2022-02-23 09:28:07 -0500

    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# AkronEarlyCollegeHighSample
+# Learn Web Development
+
+## HTML
+https://www.codecademy.com/learn/learn-html
+## JavaScript
+https://www.codecademy.com/learn/introduction-to-javascript
+## React 
+https://www.codecademy.com/learn/react-101
+
+## Static Web Apps
+https://azure.microsoft.com/en-us/services/app-service/static/#overview
+
+## Azure Student 
+https://azure.microsoft.com/en-us/free/students/
+
+## How to Use GitHub
+https://docs.github.com/en/get-started
---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './aps.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Akron Early College High Career Expo
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
